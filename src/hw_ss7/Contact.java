eae08556d1d71b5bd38aab08d4570b967be1a4d5
HoangTu <trantu2000hp@gmail.com>
2022-10-03 20:32:50 +0700

    SS7_HW

---
+++ src/hw_ss7/Contact.java
@@ -0,0 +1,31 @@
+package hw_ss7;
+
+public class Contact {
+    String name;
+    String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String toString(){
+        return getName() + " - " + getPhone();
+    }
+}
--- src/hw_ss7/Controller.java
+++ src/hw_ss7/Controller.java
@@ -1,4 +1,51 @@
 package hw_ss7;
 
+
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import java.util.ArrayList;
+
 public class Controller {
+    public TextField txtName;
+    public TextField txtPhone;
+    public Text error;
+    public Text result;
+
+    private ArrayList<Contact> contactList = new ArrayList<>();
+
+    public void handleSubmit(){
+        try{
+            error.setVisible(false);
+            if (txtName.getText().isEmpty() || txtPhone.getText().isEmpty()){
+                throw new Exception("Please fill all the fields before submit!");
+            }
+            updateContact();
+            printResult();
+        }catch(Exception e){
+            //validation form error
+            error.setText(e.getMessage());
+            error.setVisible(true);
+        }
+    }
+
+    public void printResult(){
+        String txt = "";
+        for (Contact c:contactList) {
+            txt += c.toString();
+        }
+        result.setText(txt);
+    }
+
+    public void updateContact (){
+        for (Contact c: contactList) {
+            if(c.getName().equals(txtName.getText())){
+                if(!c.getPhone().equals(txtPhone.getText())){
+                    c.setPhone(txtPhone.getText());
+                    return;
+                }
+                return;
+            }
+        }
+        contactList.add(new Contact(txtName.getText(), txtPhone.getText()));
+    }
 }
