bb48fc8d40e27b5034db77118b9179dcacde0ed9
HoangTu <trantu2000hp@gmail.com>
2022-10-04 18:33:12 +0700

    SS8_HW

--- src/hw_ss8/Controller.java
+++ src/hw_ss8/Controller.java
@@ -6,13 +6,18 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.util.Comparator;
+
 public class Controller {
     public TextField txtName;
     public TextField txtEmail;
     public TextField txtMark;
     public Text error;
     public ListView<Student> lv;
-    ObservableList<Student> studentsList = FXCollections.observableArrayList();
+
+    private boolean nameSortDsc=false;
+    private boolean markSortDsc=false;
+    private ObservableList<Student> studentsList = FXCollections.observableArrayList();
 
     public void handleSubmit(){
         try{
@@ -31,6 +36,47 @@ public class Controller {
     public void printResult(){
         lv.setItems(studentsList); //auto call toString method;
         lv.refresh();
+    }
+
+    public void sortByMark(){
+        if(!markSortDsc){
+         studentsList.sort(new Comparator<Student>() {
+             @Override
+             public int compare(Student o1, Student o2) {
+                 return o1.getMark()-o2.getMark();
+             }
+         });
+         markSortDsc=true;
+        }else{
+            studentsList.sort(new Comparator<Student>() {
+                @Override
+                public int compare(Student o1, Student o2) {
+                    return o2.getMark()-o1.getMark();
+                }
+            });
+            markSortDsc=false;
+        }
+        printResult();
+    }
+    public void sortByName(){
+        if(!nameSortDsc){
+            studentsList.sort(new Comparator<Student>() {
+                @Override
+                public int compare(Student o1, Student o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+            nameSortDsc=true;
+        }else{
+            studentsList.sort(new Comparator<Student>() {
+                @Override
+                public int compare(Student o1, Student o2) {
+                    return o2.getName().compareTo(o1.getName());
+                }
+            });
+            nameSortDsc=false;
+        }
+        printResult();
     }
 
 }
